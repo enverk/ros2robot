191e567f34d520741e65983f1f8934c53fc24ba5
Selcuk Atav <selcukatav41@hotmail.com>
2023-12-09 11:12:48 +0300

    communication with ros2

--- app/mobile_app/server/cmd/app/main.go
+++ app/mobile_app/server/cmd/app/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
-func main()  {
-	fmt.Printf("selcuk")
+func main() {
+	// mqtt broker and port number
+	broker := "tcp://192.168.1.110:1883"
+
+	// mqtt client creation
+	opts := MQTT.NewClientOptions().AddBroker(broker)
+	client := MQTT.NewClient(opts)
+
+	//mqtt client stat
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	fmt.Println("Connection with broker is successful!")
+
+	message := "Hi from selcuk's computer."
+
+	topic := "/chatter" 
+	token := client.Publish(topic, 1, false, message)
+	token.Wait()
+
+	if token.Error() != nil {
+		fmt.Println("There is been error while sending the message:", token.Error())
+	} else {
+		fmt.Println("The message is successfully deliverred.")
+	}
+
+	client.Disconnect(250)
+	fmt.Println("Mqtt client is closed")
 }
