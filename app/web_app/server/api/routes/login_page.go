8777bc47423600a5079063a6b85b7e2452582466
Selcuk Atav <selcukatav41@hotmail.com>
2024-02-13 23:43:02 +0300

    add begining of login page

---
+++ app/web_app/server/api/routes/login_page.go
@@ -0,0 +1,17 @@
+package routes
+
+// import (
+// 	"github.com/labstack/echo/v4"
+// 	"ros2.com/web_app/server/databases"
+// )
+
+// type AuthRoute struct {
+// 	Client databases.Mongo
+// }
+
+// func (a *AuthRoute) Login(c echo.Context) error {
+// 	username := c.FormValue("username")
+// 	password := c.FormValue("password")
+// 	role = c.FormValue("role")
+
+// }
--- app/web_app/server/api/routes/router.go
+++ app/web_app/server/api/routes/router.go
@@ -3,11 +3,18 @@ package routes
 import (
 	"github.com/labstack/echo/v4"
 	"ros2.com/web_app/server/api/handlers"
+	"ros2.com/web_app/server/databases"
 )
+
+type DBRoute struct{
+	Client databases.Mongo
+}
 
 func New() *echo.Echo {
 	e := echo.New()
-
+	// r:=DBRoute{
+	// 	Client:*client,
+	// }
 	e.GET("/", MainPage)
 	e.GET("/game", GetGame)
 	e.POST("/game", handlers.HandleCoordinates)
