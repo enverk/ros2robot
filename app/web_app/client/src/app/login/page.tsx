767b20c704cd13872ebbfa997043cdb7ffefd14a
ebruavsar08@gmail.com <73585933+Ebruavsar@users.noreply.github.com>
2024-02-21 10:14:26 +0300

    add react login

--- app/web_app/client/src/app/login/page.tsx
+++ app/web_app/client/src/app/login/page.tsx
@@ -2,9 +2,19 @@
 
 import React from "react";
 import { useForm } from "react-hook-form";
+import { GoogleLogin } from 'react-google-login';
+import './style.css';
 
 function page() {
   const { register, handleSubmit } = useForm();
+
+  const LoginSuccess = (res: any) => {
+    console.log(res)
+  }
+
+  const LoginFail = (res: any) => {
+    console.log(res)
+  }
 
   const onSubmit = (data: any) => {
     fetch("http://localhost:5172/login", {
@@ -19,20 +29,42 @@ function page() {
       .then((data) => {});
 
       console.log(data);
+
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Email:</label>
-        <input type="email" {...register("email")} />
+    <div className="mainContainer">
+        <div className="titleContainer">
+        <div>Kullanıcı Girişi</div>
       </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" {...register("password")} />
+      <br />
+      <hr />
+      <div className="inputContainer">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="inputContainer">
+            <label>Email:</label>
+            <input type="text" {...register("email")}
+            placeholder="Enter your email here"
+            className="inputBox" />
+          </div>
+          <br />
+          <div className="inputContainer">
+            <label>Password:</label>
+            <input type="password" {...register("password")} 
+            placeholder="Enter your password here"
+            className="inputBox"/>
+          </div>
+          <br />
+          <button type="submit" className="inputButton">Giriş Yap</button>
+        </form>
       </div>
-      <button type="submit">Giriş Yap</button>
-    </form>
+      <GoogleLogin 
+      clientId="481729939558-s74nks3clojfdup270nsmst09be3jcl0.apps.googleusercontent.com"
+      onSuccess={LoginSuccess}
+      onFailure={LoginFail}
+      />
+    </div>
+ 
   );
 }
 
