11930636c089a5c0693f35ea19c4c54b8939608e
B1ack-D3ath <dogrueyup2@gmail.com>
2024-05-08 01:39:00 +0300

    Add Arduino Mega Final Code

--- arduino/ROS2_Serial_Node_Mix/ROS2_Serial_Node_Mix.ino
+++ arduino/ROS2_Serial_Node_Mix/ROS2_Serial_Node_Mix.ino
@@ -1,3 +1,15 @@
+// Adding I2C and MPU9250 Libraries
+#include <MPU9250_WE.h>
+#include <Wire.h>
+
+// Introducing MPU9250 variable
+#define CALIBRATE_COUNT_MPU 1000
+#define MPU9250_ADDR 0x68
+
+MPU9250_WE myMPU = MPU9250_WE(MPU9250_ADDR);
+unsigned long previous_time_mpu = millis();
+int period_mpu = 10;
+
 // Introducing Stepper Motor Driver Pins
 #define motor_right_enable 23
 #define motor_right_direction 25
@@ -17,7 +29,40 @@ int period_motor_right = 10000;
 void setup() {
   // Begining Communications
   Serial.begin(115200);
-  //Serial.println("Ready");
+  Wire.begin();
+
+  // Starting MPU9250
+  if (!myMPU.init()) {}
+  if (!myMPU.initMagnetometer()) {}
+
+  // Setting MPU9250 Parameters
+  delay(CALIBRATE_COUNT_MPU);
+  myMPU.enableGyrDLPF();
+  myMPU.setGyrDLPF(MPU9250_DLPF_2);
+  myMPU.setSampleRateDivider(5);
+  myMPU.setGyrRange(MPU9250_GYRO_RANGE_250);
+  myMPU.setAccRange(MPU9250_ACC_RANGE_2G);
+  myMPU.enableAccDLPF(true);
+  myMPU.setAccDLPF(MPU9250_DLPF_2);
+  myMPU.enableAccAxes(MPU9250_ENABLE_XYZ);
+  myMPU.enableGyrAxes(MPU9250_ENABLE_XYZ);
+  myMPU.setMagOpMode(AK8963_CONT_MODE_100HZ);
+
+  // Calibrating the MPU9250
+  delay(200);
+  myMPU.accOffsetVal = { 0.f, 0.f, 0.f };
+  myMPU.gyrOffsetVal = { 0.f, 0.f, 0.f };
+  xyzFloat accelSum = { 0.f, 0.f, 0.f };
+  xyzFloat gyroSum = { 0.f, 0.f, 0.f };
+  for (int i = 0; i < CALIBRATE_COUNT_MPU; i++) {
+    accelSum += myMPU.getAccRawValues();
+    gyroSum += myMPU.getGyrRawValues();
+    delay(1);
+  }
+  accelSum /= CALIBRATE_COUNT_MPU;
+  accelSum.z -= 16384.0f;
+  myMPU.accOffsetVal = accelSum;
+  myMPU.gyrOffsetVal = gyroSum / CALIBRATE_COUNT_MPU;
 
   // Making Initial Configuration Settings of Motor Drives
   pinMode(motor_right_enable, OUTPUT);
@@ -36,10 +81,24 @@ void setup() {
 }
 
 void loop() {
+  fMpu();
   fSerialRead();
   fMotorMove();
   digitalWrite(motor_right_pwm, LOW);
   digitalWrite(motor_left_pwm, LOW);
+}
+
+// Reading MPU9250's Data in Each Period and Sending it via Serial Communication
+void fMpu() {
+  unsigned long current_time_millis = millis();
+  if (current_time_millis - previous_time_mpu > period_mpu) {
+    xyzFloat accelValues = myMPU.getGValues();
+    xyzFloat gyrValues = myMPU.getGyrValues();
+    xyzFloat magValue = myMPU.getMagValues();
+    float temp = myMPU.getTemperature();
+    float resultantG = myMPU.getResultantG(accelValues);
+    Serial.println(String(accelValues.x) + "," + accelValues.y + "," + accelValues.z + "," + gyrValues.x + "," + gyrValues.y + "," + gyrValues.z + "," + magValue.x + "," + magValue.y);
+  }
 }
 
 // Controlling Serial Communication and Reading Data
@@ -59,7 +118,7 @@ void fSerialRead() {
   }
 }
 
-// Determining the Directions of Motors
+// Determining the Direction of Motors
 void fMotorRotation() {
   if (period_motor_left < 0) {
     period_motor_left *= -1;
@@ -80,7 +139,7 @@ void fMotorRotation() {
 void fMotorMove() {
   unsigned long current_time_micros = micros();
 
-  // Left Motor Controll
+  //Left Motor Controll
   if (period_motor_left == 10000) {
     digitalWrite(motor_left_enable, HIGH);
     digitalWrite(motor_left_pwm, LOW);
--- arduino/ROS2_Serial_Publish_Imu/ROS2_Serial_Publish_Imu.ino
+++ arduino/ROS2_Serial_Publish_Imu/ROS2_Serial_Publish_Imu.ino
@@ -1,55 +1,67 @@
+// Adding I2C and MPU9250 Libraries
 #include <MPU9250_WE.h>
 #include <Wire.h>
+
+// Introducing MPU9250 variable
+#define CALIBRATE_COUNT_MPU 1000
 #define MPU9250_ADDR 0x68
 
-MPU9250_WE myMPU9250 = MPU9250_WE(MPU9250_ADDR);
+MPU9250_WE myMPU = MPU9250_WE(MPU9250_ADDR);
+unsigned long previous_time_mpu = millis();
+int period_mpu = 10;
 
 void setup() {
-  //Comunicaions begins
+  // Begining Communications
   Serial.begin(115200);
   Wire.begin();
 
-  //Start MPU9250
-  if (!myMPU9250.init()) {}
-  if (!myMPU9250.initMagnetometer()) {}
+  // Starting MPU9250
+  if (!myMPU.init()) {}
+  if (!myMPU.initMagnetometer()) {}
 
-  //Settings MPU9250 Parameters
-  delay(1000);
-  myMPU9250.enableGyrDLPF();
-  myMPU9250.setGyrDLPF(MPU9250_DLPF_2);
-  myMPU9250.setSampleRateDivider(5);
-  myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_250);
-  myMPU9250.setAccRange(MPU9250_ACC_RANGE_2G);
-  myMPU9250.enableAccDLPF(true);
-  myMPU9250.setAccDLPF(MPU9250_DLPF_2);
-  myMPU9250.enableAccAxes(MPU9250_ENABLE_XYZ);
-  myMPU9250.enableGyrAxes(MPU9250_ENABLE_XYZ);
-  myMPU9250.setMagOpMode(AK8963_CONT_MODE_100HZ);
+  // Setting MPU9250 Parameters
+  delay(CALIBRATE_COUNT_MPU);
+  myMPU.enableGyrDLPF();
+  myMPU.setGyrDLPF(MPU9250_DLPF_2);
+  myMPU.setSampleRateDivider(5);
+  myMPU.setGyrRange(MPU9250_GYRO_RANGE_250);
+  myMPU.setAccRange(MPU9250_ACC_RANGE_2G);
+  myMPU.enableAccDLPF(true);
+  myMPU.setAccDLPF(MPU9250_DLPF_2);
+  myMPU.enableAccAxes(MPU9250_ENABLE_XYZ);
+  myMPU.enableGyrAxes(MPU9250_ENABLE_XYZ);
+  myMPU.setMagOpMode(AK8963_CONT_MODE_100HZ);
 
-  //Calibrating MPU9250
+  // Calibrating the MPU9250
   delay(200);
-  myMPU9250.accOffsetVal = { 0.f, 0.f, 0.f };
-  myMPU9250.gyrOffsetVal = { 0.f, 0.f, 0.f };
+  myMPU.accOffsetVal = { 0.f, 0.f, 0.f };
+  myMPU.gyrOffsetVal = { 0.f, 0.f, 0.f };
   xyzFloat accelSum = { 0.f, 0.f, 0.f };
   xyzFloat gyroSum = { 0.f, 0.f, 0.f };
-  for (int i = 0; i < 1000; i++) {
-    accelSum += myMPU9250.getAccRawValues();
-    gyroSum += myMPU9250.getGyrRawValues();
+  for (int i = 0; i < CALIBRATE_COUNT_MPU; i++) {
+    accelSum += myMPU.getAccRawValues();
+    gyroSum += myMPU.getGyrRawValues();
     delay(1);
   }
-  accelSum /= 1000.f;
+  accelSum /= CALIBRATE_COUNT_MPU;
   accelSum.z -= 16384.0f;
-  myMPU9250.accOffsetVal = accelSum;
-  myMPU9250.gyrOffsetVal = gyroSum / 1000.f;
+  myMPU.accOffsetVal = accelSum;
+  myMPU.gyrOffsetVal = gyroSum / CALIBRATE_COUNT_MPU;
 }
 
 void loop() {
-  //Get and Serial Write MPU9250 Values With Frquency = 100
-  xyzFloat accelValues = myMPU9250.getGValues();
-  xyzFloat gyrValues = myMPU9250.getGyrValues();
-  xyzFloat magValue = myMPU9250.getMagValues();
-  float temp = myMPU9250.getTemperature();
-  float resultantG = myMPU9250.getResultantG(accelValues);
-  Serial.println(String(accelValues.x) + "," + accelValues.y + "," + accelValues.z + "," + gyrValues.x + "," + gyrValues.y + "," + gyrValues.z + "," + magValue.x + "," + magValue.y);
-  delay(10);
+  fMpu();
+}
+
+// Reading MPU9250's Data in Each Period and Sending it via Serial Communication
+void fMpu() {
+  unsigned long current_time_millis = millis();
+  if (current_time_millis - previous_time_mpu > period_mpu) {
+    xyzFloat accelValues = myMPU.getGValues();
+    xyzFloat gyrValues = myMPU.getGyrValues();
+    xyzFloat magValue = myMPU.getMagValues();
+    float temp = myMPU.getTemperature();
+    float resultantG = myMPU.getResultantG(accelValues);
+    Serial.println(String(accelValues.x) + "," + accelValues.y + "," + accelValues.z + "," + gyrValues.x + "," + gyrValues.y + "," + gyrValues.z + "," + magValue.x + "," + magValue.y);
+  }
 }
