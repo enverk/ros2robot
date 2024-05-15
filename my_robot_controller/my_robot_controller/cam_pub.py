ea425ac97097852067e4759ad0cf504dbb4403a5
B1ack-D3ath <128817169+B1ack-D3ath@users.noreply.github.com>
2024-05-15 13:49:02 +0300

    Add camera

--- my_robot_controller/my_robot_controller/cam_pub.py
+++ my_robot_controller/my_robot_controller/cam_pub.py
@@ -1,43 +1,56 @@
-#!/usr/bin/python3
-
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 import cv2
+from flask import Flask
+from flask_socketio import SocketIO
+from base64 import b64encode
+import threading
+import time
 
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-
-class VideoPublisher(Node):
-
+class VideoStreamNode(Node):
     def __init__(self):
-        super().__init__('video_publisher')
-        self.publisher_ = self.create_publisher(Image, 'video_frames', 10)
-        self.timer_period = 0.05 
-        self.timer = self.create_timer(self.timer_period, self.timer_callback)
-        self.bridge = CvBridge()
+        super().__init__('video_stream_node')
         self.cap = cv2.VideoCapture(0)
-        self.get_logger().info("Video publisher başlatıldı")
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # Daha düşük çözünürlük
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        if not self.cap.isOpened():
+            self.get_logger().error('Kamera açılamıyor')
+            exit()
+        else:
+            self.get_logger().info('Kamera başarıyla açıldı')
 
-
-
-    def timer_callback(self):
-        ret, frame = self.cap.read()
-        if ret:
-            msg = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
-            self.publisher_.publish(msg)
-
-
+    def capture_video(self):
+        frame_rate = 10  # saniyede 10 kare
+        prev = 0
+        while rclpy.ok():
+            time_elapsed = time.time() - prev
+            ret, frame = self.cap.read()
+            if not ret:
+                self.get_logger().error("Kamera'dan görüntü alınamıyor")
+                continue
+            if time_elapsed > 1./frame_rate:
+                prev = time.time()
+                _, buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
+                encoded_image = b64encode(buffer).decode('utf-8')
+                socketio.emit('video_frame', {'data': encoded_image})
 
 def main(args=None):
     rclpy.init(args=args)
-    video_publisher = VideoPublisher()
-    rclpy.spin(video_publisher)
-    video_publisher.cap.release()
-    video_publisher.destroy_node()
-    rclpy.shutdown()
+    node = VideoStreamNode()
+    flask_thread = threading.Thread(target=lambda: socketio.run(app, host='0.0.0.0', port=3002))
+    camera_thread = threading.Thread(target=node.capture_video)
+    flask_thread.start()
+    camera_thread.start()
+    try:
+        flask_thread.join()
+        camera_thread.join()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
-
     main()
-
